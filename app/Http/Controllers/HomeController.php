44b17059e6f0b311ab44c24d74c01f5f4b46b01e
SamuelAmbrosio <samuelambrosio99@gmail.com>
2020-12-18 13:45:53 -0600

    APN

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -10,6 +10,10 @@ use NotificationChannels\Apn\ApnChannel;
 use NotificationChannels\Apn\ApnMessage;
 use App\Client;
 
+use Lcobucci\JWT\Signer\Key;
+use Lcobucci\JWT\Signer\Ecdsa\Sha256;
+use Lcobucci\JWT\Configuration;
+
 class HomeController extends Controller
 {
     /**
@@ -65,13 +69,60 @@ class HomeController extends Controller
     }
 
     public function send($id){
-        $cliente = Client::find($id);
+        $keyfile = storage_path('AuthKey_LM256AQ36M.p8');               // Your p8 Key file
+        $keyid = 'LM256AQ36M';                            // Your Key ID
+        $teamid = 'HD9RGT8HFZ';                           // Your Team ID (see Developer Portal)
+        $bundleid = 'com.micoopeApp';                // Your Bundle ID
+        $url = 'https://api.push.apple.com';  // development url, or use http://api.push.apple.com for production environment
+        $token = 'f9ec3c69e3f1b37b80d81f5a5e1ee9fe0ff1960fd4dc8be93d6922a49b5f8870';              // Device Token
 
-        $this->send_notification($cliente);
+        $message = '{"aps":{"alert":"Prueba de notificacion"}}';
 
-        return $cliente->token;
+        $key = openssl_pkey_get_private('file://'.$keyfile);
 
-        return 'Hecho';
+        $header = ['alg'=>'ES256','kid'=>$keyid];
+        $claims = ['iss'=>$teamid,'iat'=>time()];
+
+        $header_encoded = $this->base64($header);
+        $claims_encoded = $this->base64($claims);
+
+        $signature = '';
+        openssl_sign($header_encoded . '.' . $claims_encoded, $signature, $key, 'sha256');
+        $jwt = $header_encoded . '.' . $claims_encoded . '.' . base64_encode($signature);
+
+        // only needed for PHP prior to 5.5.24
+        if (!defined('CURL_HTTP_VERSION_2_0')) {
+            define('CURL_HTTP_VERSION_2_0', 3);
+        }
+
+        $http2ch = curl_init();
+        curl_setopt_array($http2ch, array(
+        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_2_0,
+        CURLOPT_URL => "$url/3/device/$token",
+        CURLOPT_PORT => 443,
+        CURLOPT_HTTPHEADER => array(
+            "apns-topic: {$bundleid}",
+            "authorization: bearer $jwt"
+        ),
+        CURLOPT_POST => TRUE,
+        CURLOPT_POSTFIELDS => $message,
+        CURLOPT_RETURNTRANSFER => TRUE,
+        CURLOPT_TIMEOUT => 30,
+        CURLOPT_HEADER => 1
+        ));
+
+        $result = curl_exec($http2ch);
+        if ($result === FALSE) {
+        //throw new Exception("Curl failed: ".curl_error($http2ch));
+        echo curl_error($http2ch);
+        }
+
+        $status = curl_getinfo($http2ch, CURLINFO_HTTP_CODE);
+        echo $status;
+    }
+
+    function base64($data) {
+        return rtrim(strtr(base64_encode(json_encode($data)), '+/', '-_'), '=');
     }
 
     public function via($notifiable)
