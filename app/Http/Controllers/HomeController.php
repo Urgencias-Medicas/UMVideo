d06da29eac3758960629a200e12b9ef048a1e3b2
SamuelAmbrosio <samuelambrosio99@gmail.com>
2020-12-18 15:11:20 -0600

    test apn

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -78,7 +78,7 @@ class HomeController extends Controller
             'link' => $link
         );
 
-        return $message;
+        //return $message;
 
         //return $user;
         //return $user->os;
