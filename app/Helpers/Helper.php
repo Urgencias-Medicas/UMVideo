83ff760f9598d7ff0250a818e6f9902d2d65ea30
SamuelAmbrosio <samuelambrosio99@gmail.com>
2020-12-18 15:08:12 -0600

    test sandbox

--- app/Helpers/Helper.php
+++ app/Helpers/Helper.php
@@ -70,7 +70,7 @@ class Helper
             $keyid = 'LM256AQ36M';                            // Your Key ID
             $teamid = 'HD9RGT8HFZ';                           // Your Team ID (see Developer Portal)
             $bundleid = 'com.micoopeApp';                // Your Bundle ID
-            $url = 'https://api.push.apple.com';  // development url, or use http://api.push.apple.com for production environment
+            $url = 'https://api.sandbox.push.apple.com';  // development url, or use http://api.push.apple.com for production environment
             $token = $token;              // Device Token
 
             $message = '{"aps" : {
