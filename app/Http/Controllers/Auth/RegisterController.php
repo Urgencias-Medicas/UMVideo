abc872bbe06fdd8e51f7bd8ccb7793c6c9a23986
SamuelAmbrosio <samuelambrosio99@gmail.com>
2021-01-04 23:26:30 -0600

    Register redirect

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use App\User;
-use App\Htpp\Controllers\Auth\RegistersUsers;
+use App\Http\Controllers\Auth\RegistersUsers;
 //cuse Illuminate\Foundation\Auth\RegistersUsers;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
