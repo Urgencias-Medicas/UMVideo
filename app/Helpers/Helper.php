429bf7fcf76206979b3e5f5ec8cd5d215725bcab
SamuelAmbrosio <samuelambrosio99@gmail.com>
2021-01-07 11:04:21 -0600

    Notification helper changes

--- app/Helpers/Helper.php
+++ app/Helpers/Helper.php
@@ -33,7 +33,8 @@ class Helper
             # Our new data
             $body = array(
                 'to' => $token,
-                'notification' => $data
+                'notification' => $data,
+                'data' => array("link" => isset($data['link']) ? $data['link'] : null)
             );
 
             $payload = json_encode($body);
