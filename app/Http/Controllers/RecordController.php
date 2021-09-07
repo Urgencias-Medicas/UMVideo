22b4674db38dff68accba1148dd47318410d78bb
Devel Security <win10@devel.com>
2021-09-06 22:27:26 -0600

    Changes to be committed: 	new file:   app/Http/Controllers/RecordController.php 	new file:   app/Record.php 	new file:   database/factories/RecordFactory.php 	new file:   database/migrations/2021_09_06_211705_create_records_table.php 	new file:   database/seeds/RecordSeeder.php 	modified:   routes/api.php 	modified:   sql/local.sql

---
+++ app/Http/Controllers/RecordController.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Record;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+
+class RecordController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $records = Record::select('title', 'image', 'description')->get();
+
+        for ($i=0; $i < count($records); $i++)
+            $records[$i]->image = base64_encode(Storage::disk('local')->get("images\\records\\{$records[$i]->image}"));
+
+        return $records;
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Record  $record
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Record $record)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Record  $record
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Record $record)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Record  $record
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Record $record)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Record  $record
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Record $record)
+    {
+        //
+    }
+}
