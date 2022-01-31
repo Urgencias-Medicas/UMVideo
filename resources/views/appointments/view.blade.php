05bdf13103759d78e38e6d019318deffe513e2f2
SamuelAmbrosio99 <samuelambrosio99@gmail.com>
2022-01-31 10:46:00 -0600

    e agrega el calendario

--- resources/views/appointments/view.blade.php
+++ resources/views/appointments/view.blade.php
@@ -4,7 +4,7 @@
 <div class="container">
     <div class="card" style="height:900px; text-align: center;">
         <div class="card-body" style="padding: 0px;">
-            <iframe src="https://smartla.daily.co/{{$Appointment->roomName}}" style="height: 100%; width: 100%;"></iframe>
+            <iframe src="https://smartla.daily.co/{{$Appointment->roomName}}" allow="camera;microphone" style="height: 100%; width: 100%;"></iframe>
         </div>
     </div>
 </div>
